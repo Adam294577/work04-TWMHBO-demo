33c09d35b781afdca32ee9094be26cef72507987
skatevolley <adam29457756@gmail.com>
2024-02-06 12:29:00 +0800

    init new project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+window.onload = () =>{
+
+    const {createApp, ref, reactive, computed, watch, onMounted , onUpdated} = Vue
+    const App = {
+        setup(){
+      
+            return{}   
+        },
+
+    }
+    createApp(App).mount("#app")     
+
+}
+
+
