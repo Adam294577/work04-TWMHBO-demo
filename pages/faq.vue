87b1de64d7b9e57de82f7b50ec8f23190e663563
Satoru <adam29457756@gmail.com>
2024-02-17 22:32:41 +0800

    將 原開發的 Faq功能 至nuxt運行

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,6 +1,14 @@
-<script setup></script>
+<script setup>
+const store = useFaqStore();
+const { FaqData } = store;
+const { FaqNavIs, faqContOpenIdx } = storeToRefs(store);
+const GetFaqApidata = await useFetch("/api/faqData");
+FaqData.data = GetFaqApidata.data.value.data;
+FaqNavIs.value = "Application";
+faqContOpenIdx.value = [];
+</script>
 <template>
-  <div class="faq bg-[#EEE] relative min-h-[50vh] pt-[30px] pb-10">
+  <div class="bg-[#EEE] relative min-h-[50vh] pt-[30px] pb-10">
     <FaqNav />
     <FaqCont />
   </div>
